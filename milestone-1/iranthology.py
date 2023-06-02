a4c52c973f5372515e87b73adf79fcc0e11a4dd5
ybrenning <yannickbrenning2@gmail.com>
2023-06-02 11:39:15 +0200

    Finish milestone 2 (?)

--- milestone-1/iranthology.py
+++ milestone-1/iranthology.py
@@ -1,5 +1,5 @@
 import ir_datasets
-from ir_datasets.formats import JsonlDocs, TrecXmlQueries
+from ir_datasets.formats import JsonlDocs, TrecXmlQueries, TrecQrels
 from typing import NamedTuple
 from ir_datasets.datasets.base import Dataset
 
@@ -10,7 +10,8 @@ class IRAnthologyDocument(NamedTuple):
     def default_text(self):
         return self.text
 
-ir_datasets.registry.register('iranthology-ir-lab-sose2023-information-retrievers', Dataset(
-    JsonlDocs(ir_datasets.util.PackageDataFile(path="datasets_in_progress/milestone-1-documents.jsonl"), doc_cls=IRAnthologyDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.PackageDataFile(path="datasets_in_progress/milestone-1-topics.xml"), lang='en'),
+ir_datasets.registry.register("iranthology-ir-lab-sose2023-information-retrievers", Dataset(
+    JsonlDocs(ir_datasets.util.PackageDataFile(path="datasets_in_progress/milestone-1-documents.jsonl"), doc_cls=IRAnthologyDocument, lang="en"),
+    TrecXmlQueries(ir_datasets.util.PackageDataFile(path="datasets_in_progress/milestone-1-topics.xml"), lang="en"),
+    TrecQrels(ir_datasets.util.PackageDataFile(path="datasets_in_progress/qrels.txt"), {0: "Not Relevant", 1: "Relevant"})
 ))
